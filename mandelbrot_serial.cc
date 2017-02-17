bfa02c6825f1253ede0d6664c58ee5aeb442e680
tonyn9 <tonyn9@uci.edu>
2017-02-16 23:36:47 -0800

    Update mandelbrot_serial.cc

--- mandelbrot_serial.cc
+++ mandelbrot_serial.cc
@@ -1,49 +1,50 @@
 /**
- *  \file mandelbrot_serial.cc
- *  \brief Lab 2: Mandelbrot set serial code
+ *  \file mandelbrot_susie.cc
+ *
+ *  \brief Implement your parallel mandelbrot set in this file.
  */
-
 
 #include <iostream>
 #include <cstdlib>
+#include <mpi.h>
 
-#include "render.hh"
 
-using namespace std;
-
-#define WIDTH 1000
-#define HEIGHT 1000
-
-int
-mandelbrot(double x, double y) {
+// given mandelbrot function
+int mandelbrot(double x, double y){
   int maxit = 511;
   double cx = x;
   double cy = y;
   double newx, newy;
 
   int it = 0;
-  for (it = 0; it < maxit && (x*x + y*y) < 4; ++it) {
+  for ( it = 0; it < maxit && (x*x + y*y) < 4; ++it) {
     newx = x*x - y*y + cx;
     newy = 2*x*y + cy;
     x = newx;
-    y = newy;
+    y = newyl
   }
   return it;
 }
 
+
+
 int
-main(int argc, char* argv[]) {
+main (int argc, char* argv[])
+{
+  /* Lucky you, you get to write MPI code */
+
+  // given values from serial
   double minX = -2.1;
   double maxX = 0.7;
   double minY = -1.25;
   double maxY = 1.25;
-  
+
   int height, width;
-  if (argc == 3) {
+  if(argc == 3){
     height = atoi (argv[1]);
     width = atoi (argv[2]);
-    assert (height > 0 && width > 0);
-  } else {
+    assert (height > 0 && width 0);
+  } else{
     fprintf (stderr, "usage: %s <height> <width>\n", argv[0]);
     fprintf (stderr, "where <height> and <width> are the dimensions of the image.\n");
     return -1;
@@ -53,20 +54,24 @@ main(int argc, char* argv[]) {
   double jt = (maxX - minX)/width;
   double x, y;
 
+  // initialize MPI (from lecture)
+  MPI_Init(&argc, &argv);
 
-  gil::rgb8_image_t img(height, width);
-  auto img_view = gil::view(img);
+  // rank = process id 
+  // size = number of processors
+  int rank, size;
 
-  y = minY;
-  for (int i = 0; i < height; ++i) {
-    x = minX;
-    for (int j = 0; j < width; ++j) {
-      img_view(j, i) = render(mandelbrot(x, y)/512.0);
-      x += jt;
-    }
-    y += it;
-  }
-  gil::png_write_view("mandelbrot.png", const_view(img));
+  MPI_Comm_rank ( MPI_COMM_WORLD, &rank);
+  MPI_Comm_size ( MPI_COMM_WORLD, &size);
+
+  printf("I am %d of %d\n", rank, size);
+
+
+
+  // finish MPI block
+  MPI_Finalize();
+
+  return 0;
 }
 
 /* eof */
