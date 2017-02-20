420226e9881c2f540dad4301e7876e23fec069db
tonyn9 <tonyn9@uci.edu>
2017-02-20 01:30:28 -0800

    added basic skeleton

--- mandelbrot_ms.cc
+++ mandelbrot_ms.cc
@@ -10,6 +10,44 @@
 #include <math.h>
 #include "render.hh"
 
+// rank = process id 
+// size = number of processors
+int rank, size;
+
+// communication tags
+// these are mapped out to integers
+enum {
+  finish_tag, // no more work for slaves
+  data_tag // more work for slaves
+};
+
+
+int mpiInit(int argc, char**argv){
+  if (MPI_Init(&argc, &argv) != MPI_SUCCESS){
+    fprintf(stderr, "Failure in initialization\n");
+    return 100;
+  }
+
+  if (MPI_Comm_size(MPI_COMM_WORLD, &size) != MPI_SUCCESS){
+    fprintf(stderr, "Could not get process count\n");
+    MPI_Finalize();
+    return 101;
+  }else if (size < 2){
+    fprintf(stderr, "Must have at least 2 process\n");
+    MPI_Finalize();
+    return 102;
+  }
+
+  if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS){
+    fprintf(stderr, "Could not get rank of process\n");
+    return 103;
+  }
+
+  return 0;
+
+}
+
+
 // given mandelbrot function
 int mandelbrot(double x, double y){
   int maxit = 511;
@@ -27,6 +65,20 @@ int mandelbrot(double x, double y){
   return it;
 }
 
+// waits until told to work by root process
+// when finished, return the data
+int slave(double minX, double maxX, double minY, double maxY, double it, double jt, int height, int width){
+
+
+  return 0;
+}
+
+// gives work out to other process and collects data at the same time
+int master(double minX, double maxX, double minY, double maxY, double it, double jt, int height, int width){
+
+
+  return 0;
+}
 
 
 int
@@ -54,6 +106,24 @@ main (int argc, char* argv[])
   double jt = (maxX - minX)/width;
   double x, y;
 
+  // initialize MPI
+  if (mpi_Init(argc, argv)){
+    fprintf(stderr, "Could not init MPI\n");
+    return 10;
+  }
+
+  // split process between one process (master)
+  // and many other processes (slaves)
+
+  // master goes here
+  if (rank == 0){
+      master(minX, maxX, minY, maxY, it, jt, height, width);
+  }else{ // slave goes here
+      slave(minX, maxX, minY, maxY, it, jt, height, width);
+  }
+
+  // finish MPI block
+  MPI_Finalize();
 
   return 0;
 }
