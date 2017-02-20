7f0144e0f509a803a6d1c2fb4b185926530f5f12
tonyn9 <tonyn9@uci.edu>
2017-02-19 23:17:18 -0800

    starting

--- mandelbrot_ms.cc
+++ mandelbrot_ms.cc
@@ -6,11 +6,56 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <mpi.h>
+#include <math.h>
+#include "render.hh"
+
+// given mandelbrot function
+int mandelbrot(double x, double y){
+  int maxit = 511;
+  double cx = x;
+  double cy = y;
+  double newx, newy;
+
+  int it = 0;
+  for ( it = 0; it < maxit && (x*x + y*y) < 4; ++it) {
+    newx = x*x - y*y + cx;
+    newy = 2*x*y + cy;
+    x = newx;
+    y = newy;
+  }
+  return it;
+}
+
+
 
 int
 main (int argc, char* argv[])
 {
-  /* Lucky you, you get to write MPI code */
+  // given values from serial
+  double minX = -2.1;
+  double maxX = 0.7;
+  double minY = -1.25;
+  double maxY = 1.25;
+  int i , j;
+
+  int height, width;
+  if(argc == 3){
+    height = atoi (argv[1]);
+    width = atoi (argv[2]);
+    assert (height > 0 && width > 0);
+  } else{
+    fprintf (stderr, "usage: %s <height> <width>\n", argv[0]);
+    fprintf (stderr, "where <height> and <width> are the dimensions of the image.\n");
+    return -1;
+  }
+
+  double it = (maxY - minY)/height;
+  double jt = (maxX - minX)/width;
+  double x, y;
+
+
+  return 0;
 }
 
 /* eof */
